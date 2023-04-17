ffffdfaf80003d2e8789f71bb70ff7c513718351
Mohammed Yousef <mohammedmohammeddeeb@gmail.com>
2023-04-17 04:30:56 +0300

    Add (comments and Likes and Shares and Education) and the Events

---
+++ src/Application/Comments/Commands/CreateComment/CreateCommentCommand.cs
@@ -0,0 +1,40 @@
+﻿using MediaLink.Application.Common.Interfaces;
+using MediaLink.Domain.Entities;
+using MediaLink.Domain.Events.CommentEvents;
+using MediatR;
+
+namespace MediaLink.Application.Comments.Commands.CreateComment;
+
+public record CreateCommentCommand : IRequest<int>
+{
+    public string? Content { get; set; }
+    public int PostId { get; set; }
+    public int UserId { get; set; }
+}
+
+public class CreateCommentCommandHandler : IRequestHandler<CreateCommentCommand, int>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreateCommentCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<int> Handle(CreateCommentCommand request, CancellationToken cancellationToken)
+    {
+        var entity = new Comment
+        {
+            Content = request.Content,
+            PostId = request.PostId,
+            UserId = request.UserId
+        };
+
+        entity.AddDomainEvent(new CommentCreatedEvent(entity));
+       
+        _context.Comments.Add(entity);
+        
+        await _context.SaveChangesAsync(cancellationToken);
+       
+        return entity.Id;
+    }
+}
---
+++ src/Application/Educations/Commands/DeleteEducation/DeleteEducationCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaLink.Application.Common.Exceptions;
+using MediaLink.Application.Common.Interfaces;
+using MediaLink.Application.Educations.Commands.CreateEducation;
+using MediaLink.Domain.Entities;
+using MediaLink.Domain.Events.EducationEvents;
+using MediaLink.Domain.Events.LikeEvents;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaLink.Application.Educations.Commands.DeleteEducation;
+public record DeleteEducationCommand(int Id) : IRequest
+
+
+public class DeleteEducationCommandHandler : IRequestHandler<DeleteEducationCommand, int>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteEducationCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Unit> Handle(DeleteEducationCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.Educations
+           .Where(E => E.Id == request.Id)
+           .SingleOrDefaultAsync(cancellationToken);
+
+        if (entity == null)
+        {
+            throw new NotFoundException(nameof(entity));
+        }
+
+        _context.Educations.Remove(entity);
+
+        entity.AddDomainEvent(new EducationDeletedEvent(entity));
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
